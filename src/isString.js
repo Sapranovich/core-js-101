f821f77cf1cfe148a38b10f01a49d7aa0ca5f866
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2020-05-17 13:46:56 +0300

    add solution task

---
+++ src/isString.js
@@ -0,0 +1,13 @@
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
